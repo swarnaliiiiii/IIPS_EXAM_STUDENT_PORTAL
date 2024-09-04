b83dd5c0e223efd18ecffaf4a2790c588bccac11
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-09-04 22:52:42 +0530

    formateed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import React from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-import Login from './Login/Login'
-import Compiler from "./Compiler/Compiler"
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Login/Login";
+import Compiler from "./Compiler/Compiler";
 
 const App = () => {
   return (
     <Router>
       <Routes>
-    
         <Route path="/" element={<Login />} />
         {/* -----------------------------------------------Compiler Component */}
         <Route path="/compiler" element={<Compiler />} />
-       
       </Routes>
     </Router>
-  )
-}
+  );
+};
 
-export default App
+export default App;
