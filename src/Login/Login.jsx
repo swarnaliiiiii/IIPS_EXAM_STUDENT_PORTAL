5d8831fa5cbbc0bead9ada5dcbdf4f29841b640a
SimpPoseidon <120441879+TanishqMehrunkarIIPSDAVV@users.noreply.github.com>
2024-09-05 23:52:26 +0530

    student login

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
 import "./Login.css"; // Import the CSS file
 import logo from "../assets/iips_logo2.png"; // Corrected the import statement
+import { FaEye } from "react-icons/fa6";
+import { FaEyeSlash } from "react-icons/fa6";
 
 function Login() {
-  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
+  const [rollno, setRollNo] = useState("");
+  const [enrollno,setEnrollNo] = useState("");
+  const [subcode,setSubcode] = useState("");
+  const [date,setDate] = useState("");
+  const [d,setDisplay] = useState("none");
+
+  const toggled=()=>
+  {
+      d === "block" ? setDisplay("none") : setDisplay("block");
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     // Simulate a login process
-    if (email && password) {
+    if (name && password && rollno && enrollno && subcode && date) {
       alert("Login successful!");
       // Proceed with your login logic here
     } else {
-      alert("Please enter both email and password.");
+      alert("Please enter all fields!!!");
     }
   };
 
@@ -24,26 +36,65 @@ function Login() {
       <h2>Student: Login</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          <label>
-            Email:
-            <input
-              type="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </label>
+          <label>Name:</label>
+          <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          required
+          />
         </div>
         <div>
           <label>
             Password:
-            <input
-              type="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
+            <div className="password-eye-container">
+              <input
+                id="password-eye"
+                type={d === "block" ? "text" : "password"}
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <FaEye className="eyes-login" onClick={() => toggled()} style={{display: d}}/>
+              <FaEyeSlash className="eyes-login" onClick={() => toggled()} style={{display: (d === "block" ? "none" : "block")}}/>
+            </div>
           </label>
+        </div>
+        <div>
+            <label>Roll Number:</label>
+            <input 
+            type="text"
+            value={rollno}
+            onChange={(e) => setRollNo(e.target.value)}
+            required
+            />
+        </div>
+        <div>
+            <label>Enrollment Number:</label>
+            <input 
+            type="text"
+            value={enrollno}
+            onChange={(e) => setEnrollNo(e.target.value)}
+            required
+            />
+        </div>
+        <div>
+            <label>Subject Code:</label>
+            <input 
+            type="text"
+            value={subcode}
+            onChange={(e) => setSubcode(e.target.value)}
+            required
+            />
+        </div>
+        <div>
+            <label>Date:</label>
+            <input 
+            type="date"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            required
+            />
         </div>
         <button type="submit">Login</button>
       </form>
