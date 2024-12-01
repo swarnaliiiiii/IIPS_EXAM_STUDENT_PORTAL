8385aed4ee9244d40f9679f0dbb1a51abdc3fb16
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-12-01 20:59:51 +0530

    updated

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+# Update system packages
 apt-get update && apt-get install -y portaudio19-dev
+
+# Upgrade pip to the latest version
+python -m pip install --upgrade pip
+
+# Install Python dependencies
+pip install -r requirements.txt
