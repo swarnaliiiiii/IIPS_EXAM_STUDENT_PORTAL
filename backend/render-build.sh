5f58a49636e38c53fd0d1c9f6c49ba008e97782f
Swarnali Bhattacharyya <oli2k3@gmail.com>
2025-02-22 20:39:17 +0530

    Made render-build.sh executable

--- backend/render-build.sh
+++ backend/render-build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-# Update system packages
-apt-get update && apt-get install -y portaudio19-dev
-
-# Upgrade pip to the latest version
 python -m pip install --upgrade pip
-
-# Install Python dependencies
 pip install -r requirements.txt
+python server.py
